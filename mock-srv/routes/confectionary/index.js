00f133754ec67f93cc022a639ef7c4ef8e85c5b5
Tom Chestnut <tchestnut85@gmail.com>
2021-03-18 19:26:31 -0400

    Initial commit for the project.

---
+++ mock-srv/routes/confectionary/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = async function (fastify, opts) {
+    fastify.get('/', async function (request, reply) {
+        return [
+            {
+                id: 'B1', name: 'Chocolate Bar', rrp: '22.40',
+            }
+        ];
+    });
+};
