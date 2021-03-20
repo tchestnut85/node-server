cb3302a465ecfdf1f7a99e43f0e146264ed348b0
Tom Chestnut <tchestnut85@gmail.com>
2021-03-20 19:43:13 -0400

    Started adding websocket functionality.

--- mock-srv/app.js
+++ mock-srv/app.js
@@ -3,25 +3,27 @@
 const path = require('path');
 const AutoLoad = require('fastify-autoload');
 const cors = require('fastify-cors');
+const websocket = require('fastify-websocket');
 
 module.exports = async function (fastify, opts) {
-  // Place here your custom code!
-  fastify.register(cors);
+	// Place here your custom code!
+	fastify.register(cors);
+	fastify.register(websocket);
 
-  // Do not touch the following lines
+	// Do not touch the following lines
 
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  });
+	// This loads all plugins defined in plugins
+	// those should be support plugins that are reused
+	// through your application
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'plugins'),
+		options: Object.assign({}, opts),
+	});
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  });
+	// This loads all plugins defined in routes
+	// define your routes in one of these
+	fastify.register(AutoLoad, {
+		dir: path.join(__dirname, 'routes'),
+		options: Object.assign({}, opts),
+	});
 };
--- mock-srv/plugins/data-utils.js
+++ mock-srv/plugins/data-utils.js
@@ -1,10 +1,41 @@
 'use strict';
 const fp = require('fastify-plugin');
+const { promisify } = require('util');
+const timeout = promisify(setTimeout);
+
+const orders = {
+	A1: { total: 3 },
+	A2: { total: 7 },
+	B1: { total: 101 },
+};
 
 const catToPrefix = {
 	electronics: 'A',
 	confectionary: 'B',
 };
+
+async function* realtimeOrdersSimulator() {
+	const ids = Object.keys(orders);
+
+	while (true) {
+		const delta = Math.floor(Math.random() * 7 + 1);
+		const id = ids[Math.floor(Math.random() * ids.length)];
+		orders[id].total += delta;
+		const { total } = orders[id];
+		yield JSON.stringify({ id, total });
+		await timeout(1500);
+	}
+}
+
+function* currentOrders(category) {
+	const idPrefix = catToPrefix[category];
+	if (!idPrefix) return;
+	const ids = Object.keys(orders).filter(id => id[0] === idPrefix);
+
+	for (const id of ids) {
+		yield JSON.stringify({ id, ...orders[id] });
+	}
+}
 
 const calculateID = (idPrefix, data) => {
 	const sorted = [...new Set(data.map(({ id }) => id))];
